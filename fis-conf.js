6c4434b2cc93d3607b27b18c913c8455faf15cf4
Rambo Yang <rambogototravel@gmail.com>
2017-07-24 18:18:27 +0800

    补充DEMO

--- fis-conf.js
+++ fis-conf.js
@@ -1,1 +1,23 @@
 
+fis.config.merge({
+    deploy: {
+        remote: {
+            receiver: 'http://10.1.23.109/receiver.php',
+            from: '/static',
+            to: 'F:\\nfis\\www'
+        },
+        remote2: [
+            {
+                receiver: 'http://10.1.23.109/receiver.php',
+                from: '/static',
+                to: 'F:\\nfis\\www'
+            },
+            {
+                receiver: 'http://10.1.23.109/receiver.php',
+                from: '/WEB-INF',
+                to: 'F:\\nfis\\www\\WEB-INF',
+                subOnly: true
+            }
+        ]
+    }
+});
